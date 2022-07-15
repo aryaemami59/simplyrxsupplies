2066f528367916fb2e66e4d9757aa96220ce5628
Arya Emami <aryaemami59@yahoo.com>
2022-07-15 18:16:57 -0500

    separated the navbar component into its own folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import "./App.css";
 import {
-  Navbar,
-  NavbarBrand,
-  NavbarToggler,
-  Collapse,
-  Nav,
-  Offcanvas,
-  OffcanvasHeader,
-  OffcanvasBody,
+  // Navbar,
+  // NavbarBrand,
+  // NavbarToggler,
+  // Collapse,
+  // Nav,
+  // Offcanvas,
+  // OffcanvasHeader,
+  // OffcanvasBody,
   Col,
   Row,
   Container,
@@ -16,84 +16,79 @@ import { useState, useRef, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import items from "./data/items.json";
-import VendorDropDownsList from "./features/components/DropDownComponents/VendorDropDownsList";
-import VendorAccordionList from "./features/components/AccordionComponents/VendorAccordionList";
+// import VendorDropDownsList from "./features/components/DropDownComponents/VendorDropDownsList";
+// import VendorAccordionList from "./features/components/AccordionComponents/VendorAccordionList";
 import VendorColumnList from "./features/components/ColumnComponents/VendorColumnList";
 import InputGroupComponent from "./features/components/InputComponents/InputGroupComponent";
+import NavbarComponent from "./features/components/NavbarComponents/NavbarComponent";
 
 function App() {
-  const [show, setShow] = useState(false);
+  // const [show, setShow] = useState(false);
   const [itemsAdded, setItemsAdded] = useState([]);
   const [classes, setClasses] = useState("");
-  // const [added, setAdded] = useState(false);
   const isInitialMount = useRef(true);
 
   useEffect(() => {
     if (isInitialMount.current) {
       isInitialMount.current = false;
     } else {
-      // setAdded(true);
       setClasses("text-decoration-line-through");
     }
-    // isInitialMount.current
-    //   ? (isInitialMount.current = false)
-    //   : setClasses("text-decoration-line-through");
-    // setAdded(true);
-    // console.log(added);
   }, [itemsAdded]);
-  // console.log(added);
 
   return (
     <div className="App">
-      <div>
-        <Navbar color="dark" dark expand="lg" sticky="top">
-          <NavbarBrand href="/" className="me-3">
-            Simply Supplies
-          </NavbarBrand>
-          <NavbarToggler
-            onClick={() => setShow(!show)}
-            className="me-2"></NavbarToggler>
-          <Collapse navbar>
-            <Nav className="me-auto" navbar>
-              <VendorDropDownsList
-                classes={classes}
-                // added={added}
-                items={items}
-                itemsAdded={itemsAdded}
-                onAdd={ev => setItemsAdded([...itemsAdded, ev])}
-              />
-            </Nav>
-          </Collapse>
-          <Offcanvas
-            direction="start"
-            isOpen={show}
-            toggle={() => setShow(!show)}>
-            <OffcanvasHeader toggle={() => setShow(!show)}>
-              Offcanvas
-            </OffcanvasHeader>
-            <OffcanvasBody>
-              <strong>This is the Offcanvas body.</strong>
-              <VendorAccordionList
-                classes={classes}
-                itemsAdded={itemsAdded}
-                // added={added}
-                items={items}
-                onAdd={ev => setItemsAdded([...itemsAdded, ev])}
-              />
-            </OffcanvasBody>
-          </Offcanvas>
-        </Navbar>
-        <Container>
-          <Row className="my-5">
-            <Col md="6">
-              <InputGroupComponent />
-            </Col>
-            <Col md="4">
-              <VendorColumnList itemsAdded={itemsAdded} />
-            </Col>
-          </Row>
-        </Container>
-      </div>
+      <NavbarComponent
+        classes={classes}
+        items={items}
+        itemsAdded={itemsAdded}
+        onAdd={ev => setItemsAdded([...itemsAdded, ev])}
+      />
+      {/* <Navbar color="dark" dark expand="lg" sticky="top">
+        <NavbarBrand href="/" className="me-3">
+          Simply Supplies
+        </NavbarBrand>
+        <NavbarToggler
+          onClick={() => setShow(!show)}
+          className="me-2"></NavbarToggler>
+        <Collapse navbar>
+          <Nav className="me-auto" navbar>
+            <VendorDropDownsList
+              classes={classes}
+              items={items}
+              itemsAdded={itemsAdded}
+              onAdd={ev => setItemsAdded([...itemsAdded, ev])}
+            />
+          </Nav>
+        </Collapse>
+        <Offcanvas
+          direction="start"
+          isOpen={show}
+          toggle={() => setShow(!show)}>
+          <OffcanvasHeader toggle={() => setShow(!show)}>
+            Offcanvas
+          </OffcanvasHeader>
+          <OffcanvasBody>
+            <strong>This is the Offcanvas body.</strong>
+            <VendorAccordionList
+              classes={classes}
+              itemsAdded={itemsAdded}
+              items={items}
+              onAdd={ev => setItemsAdded([...itemsAdded, ev])}
+            />
+          </OffcanvasBody>
+        </Offcanvas>
+      </Navbar> */}
+      <Container>
+        <Row className="my-5">
+          <Col md="6">
+            <InputGroupComponent />
+          </Col>
+          <Col md="4">
+            <VendorColumnList itemsAdded={itemsAdded} />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
---
+++ src/features/components/NavbarComponents/NavbarComponent.jsx
@@ -0,0 +1,59 @@
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  Collapse,
+  Nav,
+  Offcanvas,
+  OffcanvasHeader,
+  OffcanvasBody,
+} from "reactstrap";
+import { useState } from "react";
+import VendorDropDownsList from "../DropDownComponents/VendorDropDownsList";
+import VendorAccordionList from "../AccordionComponents/VendorAccordionList";
+
+function NavbarComponent(props) {
+  const [show, setShow] = useState(false);
+
+  return (
+    <>
+      <Navbar color="dark" dark expand="lg" sticky="top">
+        <NavbarBrand href="/" className="me-3">
+          Simply Supplies
+        </NavbarBrand>
+        <NavbarToggler
+          onClick={() => setShow(!show)}
+          className="me-2"></NavbarToggler>
+        <Collapse navbar>
+          <Nav className="me-auto" navbar>
+            <VendorDropDownsList
+              classes={props.classes}
+              items={props.items}
+              itemsAdded={props.itemsAdded}
+              onAdd={props.onAdd}
+            />
+          </Nav>
+        </Collapse>
+        <Offcanvas
+          direction="start"
+          isOpen={show}
+          toggle={() => setShow(!show)}>
+          <OffcanvasHeader toggle={() => setShow(!show)}>
+            Offcanvas
+          </OffcanvasHeader>
+          <OffcanvasBody>
+            <strong>This is the Offcanvas body.</strong>
+            <VendorAccordionList
+              classes={props.classes}
+              itemsAdded={props.itemsAdded}
+              items={props.items}
+              onAdd={props.onAdd}
+            />
+          </OffcanvasBody>
+        </Offcanvas>
+      </Navbar>
+    </>
+  );
+}
+
+export default NavbarComponent;
