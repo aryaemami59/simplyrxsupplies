7c70c832069f50c48cb6d727f456dd8ef8f4ed55
Arya Emami <aryaemami59@yahoo.com>
2022-08-10 09:05:30 -0500

    fixed itemName component

--- src/features/components/ColumnComponents/ItemNameComponent.jsx
+++ src/features/components/ColumnComponents/ItemNameComponent.jsx
@@ -1,17 +1,15 @@
-import { forwardRef, memo, useCallback, useRef, useState } from "react";
+import { memo, useCallback, useRef, useState } from "react";
 // import { ListGroupItem } from "reactstrap";
-import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+// import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 import Tooltip from "react-bootstrap/Tooltip";
 import ListGroup from "react-bootstrap/ListGroup";
-import Overlay from 'react-bootstrap/Overlay'
+import Overlay from "react-bootstrap/Overlay";
 
 const renderTooltip = props => (
   <Tooltip id="button-tooltip1" {...props}>
     Copied Item Name!
   </Tooltip>
 );
-
-// const MyListItem = forwardRef((props, ref) => <ListGroupItem {...props} />);
 
 function ItemNameComponent({ vendorName, id, itemObj }) {
   const ref = useRef(null);
@@ -22,31 +20,12 @@ function ItemNameComponent({ vendorName, id, itemObj }) {
   const copyItemName = useCallback(() => {
     toggle();
     navigator.clipboard.writeText(itemObj.name);
-    setTimeout(toggle, 600);
+    setTimeout(toggle, 500);
   }, [itemObj.name, toggle]);
 
-  // const copyItemName = useCallback(() => {
-  //   toggle();
-  //   navigator.clipboard.writeText(itemObj.name);
-  //   setTimeout(toggle, 800);
-  // }, [toggle, itemObj.name]);
-
   return (
-    <OverlayTrigger
-      placement="top"
-      delay={{ show: 0, hide: 100 }}
-      overlay={renderTooltip}
-      show={tooltipOpen}
-      trigger="click">
+    <>
       <ListGroup.Item
-        id={id}
-        role="button"
-        onClick={copyItemName}
-        color="success"
-        key={`${itemObj.name}-${vendorName}-VendorColumn-ListGroupItem-name`}>
-        Item Name: {itemObj.name}
-      </ListGroup.Item>
-      {/* <MyListItem
         ref={ref}
         id={id}
         role="button"
@@ -54,20 +33,20 @@ function ItemNameComponent({ vendorName, id, itemObj }) {
         color="success"
         key={`${itemObj.name}-${vendorName}-VendorColumn-ListGroupItem-name`}>
         Item Name: {itemObj.name}
-      </MyListItem> */}
-      {/* <ListGroupItem
-          nodeRef={ref}
-          id={id}
-          role="button"
-          onClick={copyItemName}
-          color="success"
-          key={`${itemObj.name}-${vendorName}-VendorColumn-ListGroupItem-name`}>
-          Item Name: {itemObj.name}
-        </ListGroupItem> */}
-      {/* <Tooltip isOpen={tooltipOpen} target={id}>
-          Copied Item Name!
-        </Tooltip> */}
-    </OverlayTrigger>
+      </ListGroup.Item>
+      <Overlay
+        target={ref.current}
+        show={tooltipOpen}
+        placement="top"
+        // transition={false}
+      >
+        {props => (
+          <Tooltip id="overlay-example" {...props}>
+            Copied Item Name!
+          </Tooltip>
+        )}
+      </Overlay>
+    </>
   );
 }
 
