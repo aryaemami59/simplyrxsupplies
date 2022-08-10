01bf1cf7b64417e343807d23e6f2eba889713068
Arya Emami <aryaemami59@yahoo.com>
2022-08-10 01:01:10 -0500

    fixed sidebar accordion to not re-render all other accordion items when opening one

--- src/features/components/SideBarNavComponents/SideBarAccordion.jsx
+++ src/features/components/SideBarNavComponents/SideBarAccordion.jsx
@@ -1,43 +1,86 @@
-import { memo } from "react";
+import { memo, useCallback, useRef, useState } from "react";
 import {
   AccordionBody,
   AccordionHeader,
   AccordionItem,
   ListGroup,
+  Collapse,
+  Button,
+  Card,
+  CardBody,
 } from "reactstrap";
 import SingleSideBarAccordionListItem from "./SingleSideBarAccordionListItem";
-
+const collapsed = "collapsed";
 function SideBarAccordion({ items, targetId }) {
+  const [open, setOpen] = useState(false);
+  const toggle = useCallback(() => {
+    setOpen(prev => !prev);
+  }, []);
+
+  const nodeRef = useRef(null);
+
   return (
-    <AccordionItem
-      key={`${targetId}-accordion-item`}
-      // className="bg-dark bg-gradient"
-    >
-      <AccordionHeader
-        color="dark"
-        // className="bg-dark bg-gradient"
-        key={`${targetId}-accordion-header`}
-        targetId={targetId}>
-        {targetId}
-      </AccordionHeader>
-      <AccordionBody
-        // className="bg-dark bg-gradient"
-        key={`${targetId}-accordion-body`}
-        accordionId={targetId}>
-        <ListGroup>
-          {items
-            .filter(({ nav }) => nav.includes(targetId))
-            .map(f => (
-              <SingleSideBarAccordionListItem
-                items={items}
-                targetId={targetId}
-                itemObj={f}
-                key={`${f.name}-SingleSideBarAccordionListItem`}
-              />
-            ))}
-        </ListGroup>
-      </AccordionBody>
-    </AccordionItem>
+    <div>
+      <h2 className="accordion-header">
+        {/* <button
+          className={`accordion-button ${open ? "" : collapsed}`}
+          data-bs-toggle={"collapse"}
+          data-bs-target={targetId}
+          onClick={toggle}>
+          {targetId}
+        </button> */}
+        <Button
+          onClick={toggle}
+          color="light"
+          className={`accordion-button ${open ? "" : collapsed}`}>
+          {targetId}
+        </Button>
+      </h2>
+      <Collapse
+        id={targetId}
+        isOpen={open}
+        ref={nodeRef}
+        className="accordion-collapse">
+        <Card>
+          <CardBody>
+            <ListGroup>
+              {items
+                .filter(({ nav }) => nav.includes(targetId))
+                .map(f => (
+                  <SingleSideBarAccordionListItem
+                    items={items}
+                    targetId={targetId}
+                    itemObj={f}
+                    key={`${f.name}-SingleSideBarAccordionListItem`}
+                  />
+                ))}
+            </ListGroup>
+          </CardBody>
+        </Card>
+      </Collapse>
+    </div>
+    // <AccordionItem key={`${targetId}-accordion-item`}>
+    //   <AccordionHeader
+    //     color="dark"
+    //     key={`${targetId}-accordion-header`}
+    //     targetId={targetId}>
+    //     {targetId}
+    //   </AccordionHeader>
+    //   <AccordionBody key={`${targetId}-accordion-body`} accordionId={targetId}>
+    //     <ListGroup>
+    //       {items
+    //         .filter(({ nav }) => nav.includes(targetId))
+    //         .map(f => (
+    //           <SingleSideBarAccordionListItem
+    //             items={items}
+    //             targetId={targetId}
+    //             itemObj={f}
+    //             key={`${f.name}-SingleSideBarAccordionListItem`}
+    //           />
+    //         ))}
+    //     </ListGroup>
+    //   </AccordionBody>
+    // </AccordionItem>
   );
 }
 
--- src/features/components/SideBarNavComponents/SideBarAccordionList.jsx
+++ src/features/components/SideBarNavComponents/SideBarAccordionList.jsx
@@ -1,32 +1,30 @@
-import { memo, useState } from "react";
-import { Accordion } from "reactstrap";
+import { memo, useCallback, useState } from "react";
+import { Accordion, Collapse } from "reactstrap";
 import navList from "../../../data/navList";
 import SideBarAccordion from "./SideBarAccordion";
 
 const empty = [];
 
 function SideBarAccordionList({ items }) {
-  const [open, setOpen] = useState(empty);
-  // const renders = useRef(0);
-  // console.log("renders:", renders.current++);
+  // const [open, setOpen] = useState(false);
+  // const toggle = useCallback(() => {
+  //   setOpen(prev => !prev);
+  // }, []);
+  // const [open, setOpen] = useState(empty);
 
-  const toggle = id => {
-    if (open.includes(id)) {
-      setOpen(prev => {
-        const newOpen = prev.filter(e => e !== id);
-        return newOpen.length ? newOpen : empty;
-      });
-    } else {
-      setOpen(prev => prev.concat(id));
-    }
-  };
+  // const toggle = id => {
+  //   if (open.includes(id)) {
+  //     setOpen(prev => {
+  //       const newOpen = prev.filter(e => e !== id);
+  //       return newOpen.length ? newOpen : empty;
+  //     });
+  //   } else {
+  //     setOpen(prev => prev.concat(id));
+  //   }
+  // };
 
   return (
-    <Accordion
-      // className="bg-dark bg-gradient"
-      open={open}
-      toggle={toggle}
-      key={`sidebar-accordion`}>
+    <div>
       {navList.map(e => (
         <SideBarAccordion
           items={items}
@@ -34,7 +32,19 @@ function SideBarAccordionList({ items }) {
           key={`${e}-sidebaraccordion`}
         />
       ))}
-    </Accordion>
+    </div>
+    // <Accordion
+    //   open={open}
+    //   toggle={toggle}
+    //   key={`sidebar-accordion`}>
+    //   {navList.map(e => (
+    //     <SideBarAccordion
+    //       items={items}
+    //       targetId={e}
+    //       key={`${e}-sidebaraccordion`}
+    //     />
+    //   ))}
+    // </Accordion>
   );
 }
 
