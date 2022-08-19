722f2c6654f41fb384faef9f15392bff215897f5
Arya Emami <aryaemami59@yahoo.com>
2022-08-18 20:38:55 -0500

    testing deployment

---
+++ fetchInfo.js
@@ -0,0 +1,18 @@
+const AUTH_TOKEN = "Bearer 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";
+
+export const GITHUB_URL_ITEMS =
+  "https://api.github.com/repos/aryaemami59/simplysuppliesAPI/contents/items.json";
+
+export const GITHUB_URL_VENDORS =
+  "https://api.github.com/repos/aryaemami59/simplysuppliesAPI/contents/vendors.json";
+
+export const GITHUB_URL_NAVLIST =
+  "https://api.github.com/repos/aryaemami59/simplysuppliesAPI/contents/navList.json";
+
+export const FETCH_CONFIG = {
+  method: "GET",
+  headers: {
+    Accept: "application/vnd.github.v3.raw.json",
+    Authorization: AUTH_TOKEN,
+  },
+};
