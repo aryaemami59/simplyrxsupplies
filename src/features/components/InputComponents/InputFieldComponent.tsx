390b49e96b0eb1bdaec2f269f1fa495bbc9ff5a8
Arya Emami <aryaemami59@yahoo.com>
2022-08-30 21:50:32 -0500

    fixed some types

--- src/features/components/InputComponents/InputFieldComponent.tsx
+++ src/features/components/InputComponents/InputFieldComponent.tsx
@@ -1,5 +1,5 @@
 import { Form } from "react-bootstrap";
-import { memo, useState, useCallback, useRef, FC } from "react";
+import { memo, useState, useCallback, useRef, FC, ChangeEvent } from "react";
 import { shallowEqual } from "react-redux";
 import {
   clearListItems,
@@ -14,17 +14,15 @@ import { itemInterface } from "../../../addedSlice";
 
 const empty = [];
 
-function sortResults(
+const sortResults = (
   searchTerm: itemInterface,
   re: RegExp,
   trimmedValue: string
-): number {
-  // console.log(re.test(searchTerm));
+): number => {
   if (searchTerm.name.toLowerCase() === trimmedValue) {
     return 100;
   }
   if (searchTerm.name.toLowerCase().startsWith(trimmedValue)) {
-    // console.log(searchTerm.name.toLowerCase(), 75);
     return 75;
   }
   if (searchTerm.name.toLowerCase().includes(trimmedValue)) {
@@ -34,7 +32,7 @@ function sortResults(
     return searchTerm.name.toLowerCase().match(re)!.length;
   }
   return 0;
-}
+};
 
 const InputFieldComponent: FC = (): JSX.Element => {
   const items = useAppSelector(selectItemsArr, shallowEqual);
@@ -49,12 +47,12 @@ const InputFieldComponent: FC = (): JSX.Element => {
   const [val, setVal] = useState("");
 
   const listItemsFunc = useCallback(
-    e => {
-      const trimmedValue = e.target.value
+    (e: ChangeEvent<HTMLInputElement>) => {
+      const trimmedValue: string = e.target.value
         .trim()
         .toLowerCase()
         .replace(/\s{2,}/, " ");
-      const reg = trimmedValue
+      const reg: string = trimmedValue
         .split(/\s+/gi)
         .map((f: string, i: number, arr: string[]) =>
           i !== arr.length - 1 ? `(\\b(${f})+\\b)` : `(\\b(${f}))`
@@ -64,63 +62,10 @@ const InputFieldComponent: FC = (): JSX.Element => {
         .split(/\s+/gi)
         .map((f: string) => `(?=.*${f})`)
         .join("");
-      const re = new RegExp(`${reg}|${looseReg}`, "gi");
-      // console.log(e.target.value.trim().toLowerCase().match())
-      // console.log("relion insulin syringes".split(" "));
-      // console.log(trimmedValue.split(" "));
-      // console.log(
-      //   "relion insulin syringes".split(" ").includes(trimmedValue.split(" "))
-      // );
-      console.log(re);
-      // const jj = items.map(({ name }) => name.split(" "));
-      // const splitVal = trimmedValue.split(" ");
-      // const itemNames = items.map(({ name }) => name);
-
-      // for (const val of splitVal) {
-      //   console.log(val);
-      //   console.log(itemNames.filter(e => e.toLowerCase().includes(val)));
-      //   console.log(jj.filter(e => e.some(f => f === val)));
-      //   console.log(jj.filter(e => e.every(f => f === val)));
-      //   console.log(jj.filter(e => e.some(f => f.includes(val))));
-      //   console.log(jj.filter(e => e.every(f => f.includes(val))));
-      // }
-      // console.log(items.filter(({name}) => name.split(" ").every((e) => e.includes())))
-      // console.log(splitVal);
-      // console.log(
-      //   items.filter(({ name }) => [...name.toLowerCase().trim().matchAll(re)])
-      // );
-      // console.log(
-      //   items
-      //     .filter(({ name }) => re.test(name.toLowerCase().trim()))
-      //     .map(({ name }) => name)
-      // );
-      // console.log(
-      //   items
-      //     .filter(({ name }) => name.toLowerCase().trim().match(re))
-      //     .map(({ name }) => name)
-      // );
-
-      const testArr = items
-        .filter(({ name }) => re.test(name.toLowerCase().trim()))
-        .map(({ name }) => name);
-
-      const matchArr = items
-        .filter(({ name }) => name.toLowerCase().trim().match(re))
-        .map(({ name }) => name);
-
-      // console.log(matchArr);
-      const intersectedArr = matchArr.filter(e => !testArr.includes(e));
-      // console.log(intersectedArr);
-      // console.log(intersectedArr.filter(e => e.match(re)));
-      // console.log([
-      //   ..."Nitrile Small Exam Gloves CareMates Brand 100 Count".matchAll(re),
-      // ]);
-      // console.log(intersectedArr.filter(e => e.match(re))[0].match(re));
-      // console.log("4 oz Liquid Bottles".match(re));
+      const re: RegExp = new RegExp(`${reg}|${looseReg}`, "gi");
       return trimmedValue
         ? items
             .filter(({ name }) => name.toLowerCase().trim().match(re))
-            // .filter(({ name }) => re.test(name.toLowerCase().trim()))
             .slice(0, 100)
             .sort(
               (a, b) =>
@@ -133,8 +78,8 @@ const InputFieldComponent: FC = (): JSX.Element => {
   );
 
   const changeVal = useCallback(
-    e => {
-      const listItems = listItemsFunc(e);
+    (e: ChangeEvent<HTMLInputElement>): void => {
+      const listItems: itemInterface[] = listItemsFunc(e);
       setVal(e.target.value);
       dispatch(setListItems(listItems));
     },
