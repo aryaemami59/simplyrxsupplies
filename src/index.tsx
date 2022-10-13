4b79b6e49d87ec143f8b3ba7d215b15a6e54ea9c
Arya Emami <aryaemami59@yahoo.com>
2022-10-13 13:47:59 -0500

    fixing input

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { whyDidYouUpdate } from "why-did-you-update";
+// import { whyDidYouUpdate } from "why-did-you-update";
 import App from "./App";
 import "./index.css";
 import { store } from "./Redux/store";
@@ -13,7 +13,7 @@ import { store } from "./Redux/store";
 
 const container = document.getElementById("root") as HTMLDivElement;
 const root = createRoot(container);
-whyDidYouUpdate(React);
+// whyDidYouUpdate(React);
 
 root.render(
   <React.StrictMode>
