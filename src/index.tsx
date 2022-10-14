844c70205dbe4504531e8a031c4f1f0bd363a104
Arya Emami <aryaemami59@yahoo.com>
2022-10-14 12:45:55 -0500

    resolved merge conflicts

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,12 @@
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-<<<<<<< HEAD
-// import { whyDidYouUpdate } from "why-did-you-update";
-=======
 import React from "react";
->>>>>>> mu
 import App from "./App";
 import "./index.css";
 import { store } from "./Redux/store";
 
 const container = document.getElementById("root") as HTMLDivElement;
 const root = createRoot(container);
-<<<<<<< HEAD
-// whyDidYouUpdate(React);
-=======
->>>>>>> mu
 
 root.render(
   <React.StrictMode>
