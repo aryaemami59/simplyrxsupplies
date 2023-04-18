9fc6059328bba519653ce627fce86780c929c634
Arya Emami <aryaemami59@yahoo.com>
2023-04-17 23:37:24 -0500

    migrated from react-scripts to vite

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,4 +1,4 @@
-/// <reference types="react-scripts" />
+/// <reference types="vite/client" />
 import "@mui/material/styles";
 
 declare module "@mui/material/styles" {
---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    port: 3000,
+    open: "http://localhost:3000/",
+  },
+  plugins: [react()],
+});
