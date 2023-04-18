df5851d9cc4f0d45808dc4f2d430abed319bea77
Arya Emami <aryaemami59@yahoo.com>
2023-04-18 03:07:44 -0500

    deploying

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
-import { store } from "./redux/store.ts";
+import { store } from "./redux/store";
 
 const container = document.getElementById("root") as HTMLDivElement;
 const root = createRoot(container);
