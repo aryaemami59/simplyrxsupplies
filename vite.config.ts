d264820ff4eaac9e12f1b460114550cda6214322
Arya Emami <aryaemami59@yahoo.com>
2023-04-24 23:53:33 -0500

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { visualizer } from "rollup-plugin-visualizer";
+import { PluginOption, defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +9,14 @@ export default defineConfig({
     open: "http://localhost:3000/",
   },
   // base: "/simplyrxsupplies/",
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      template: "treemap", // or sunburst
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+      filename: "analyze.html",
+    }) as PluginOption,
+  ],
 });
