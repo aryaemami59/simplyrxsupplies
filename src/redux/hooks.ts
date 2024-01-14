4965d1ce1671e565efceede0b1061c867a1a00fb
Arya Emami <aryaemami59@yahoo.com>
2024-01-14 02:58:52 -0600

    Switch typed hooks to use the new `withTypes` API

--- src/redux/hooks.ts
+++ src/redux/hooks.ts
@@ -1,7 +1,6 @@
-import type { TypedUseSelectorHook } from "react-redux";
 import { useDispatch, useSelector } from "react-redux";
 
 import type { AppDispatch, RootState } from "./store";
 
-export const useAppDispatch = useDispatch<AppDispatch>;
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+export const useAppSelector = useSelector.withTypes<RootState>();
